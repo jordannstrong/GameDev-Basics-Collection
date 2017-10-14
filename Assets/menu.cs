d1aeb0a3c6128cdb1a20d71cea0fa0cf3c5bfa57
jordannstrong <jordannstrong@gmail.com>
2017-10-13 22:33:38 -0400

    changes

---
+++ Assets/menu.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+
+public class menu : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void changeScene() {
+		string name = gameObject.name;
+		if (name == "Egg") {
+			SceneManager.LoadScene (1);
+		} else if (name == "Jump") {
+			SceneManager.LoadScene (2);
+		} else if (name == "Marble") {
+			SceneManager.LoadScene (3);
+		}
+	}
+}
